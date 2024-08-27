60ca099d08c3b9a3e19983be09ca6ee630a62e08
Rohan Jeendgar <coolbirb2309@gmail.com>
2024-08-27 17:00:03 +0530

    Add L1 Dev-Assignment

---
+++ Development-Assignment/L1/client.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+import requests
+import os
+import json
+import time
+
+prompt_arr = open(r'./input.txt', 'r').readlines()
+key = os.getenv("GROQ_API_KEY")
+model = os.getenv("MODEL") or "gemma2-9b-it"
+
+def make_req(prompt):
+    start = time.time()
+    data = {
+        "messages": [{
+            "role": "user",
+            "content": prompt
+        }],
+        "model": model
+    }
+
+    req = requests.post('https://api.groq.com/openai/v1/chat/completions',
+                        headers={
+                            "Content-Type": "application/json",
+                            "Authorization": "Bearer %s" % (key)
+                            },
+                        data=json.dumps(data)
+                        )
+
+    return [prompt, json.loads(req.content), start, time.time()]
+
+
+# res = [json.loads(make_req([x])) for x in create_dict(prompt_arr, True)]
+# choices = [x['choices']['message']['content'] for x in res]
+# print(res[1])
+#
+reqs = [make_req(x) for x in prompt_arr]
+res = [{
+    "Prompt": x[0],
+    "Message": x[1]['choices'][0]['message']['content'] or x[1],
+    "TimeSent": x[2],
+    "TimeRecvd": x[3],
+    "Source": model
+} for x in reqs]
+
+print(res)
+
+with open('output.json', 'w') as f:
+    json.dump(res, f, ensure_ascii=False, indent=4)
